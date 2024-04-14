139c293a294d1a2bdb3a5196733d502f84debe71
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-04-14 19:26:11 +0200

    Project section.

---
+++ components/projects.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React from 'react';
+
+import { projectsData } from '@/lib/data';
+
+import Project from '@/components/projet';
+import SectionHeading from '@/components/section-heading';
+
+const Projects = () => {
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
