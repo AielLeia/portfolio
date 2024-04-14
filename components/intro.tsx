ce227690730b7280d68d8a77a6970067fa695435
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-04-14 18:07:51 +0200

    Intro section.

---
+++ components/intro.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { HiDownload } from 'react-icons/hi';
+
+const Intro = () => {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              alt="Avatar"
+              className="h-24 w-24 rounded-full border-[0.35rem] bo rder-white object-cover shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.2,
+              duration: 0.7,
+            }}
+            className="text-4xl absolute bottom-0 right-0"
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      >
+        <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{' '}
+        <span className="font-bold">full-stack developer</span> with{' '}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{' '}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.1 }}
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
+      >
+        <Link
+          href="#about"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition-all" />
+        </Link>
+
+        <a
+          href="/CV.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/20"
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition-all" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/isma%C3%ABl-mohamed-bouh-6b16611b6/"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950 active:scale-105 transition border border-black/20"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/AielLeia"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/20"
+        >
+          <FaGithub />
+        </a>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Intro;
